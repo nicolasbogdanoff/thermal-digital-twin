2a602dde6987264a352077026952ea3bd89936df
Nicolás Mauricio Bogdanoff <nicolasbogdanoff@gmail.com>
2026-08-14 14:35:44 -0300

    Add public thermal twin API

---
+++ thermal_digital_twin/thermal_digital_twin/__init__.py
@@ -0,0 +1,13 @@
+from .model import (
+    ThermalParameters,
+    fit_parameters,
+    generate_observations,
+    simulate_temperature,
+)
+
+__all__ = [
+    "ThermalParameters",
+    "fit_parameters",
+    "generate_observations",
+    "simulate_temperature",
+]
